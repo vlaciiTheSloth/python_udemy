f3fa27d226e5b9e9b429c3f69ce75e276a1d3e62
vlaciiTheSloth <varszegilacko@gmail.com>
2024-11-29 16:51:55 +0100

    Start of implementing previously completed chapters

---
+++ Section_1.py
@@ -0,0 +1,6 @@
+# only the main project because of migrating
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your brand name could be: " + city + " " + pet)
