72e0c83ea4bc4cef324cac12328f7759c3f9c1fa
vlaciiTheSloth <varszegilacko@gmail.com>
2024-12-09 16:30:20 +0100

    Start of section 7

---
+++ Section_7.py
@@ -0,0 +1,15 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = str(input("Take a guess! ")).lower()
+print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
