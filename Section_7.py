b52de496fca35548d658e465068ae182b3564a5a
vlaciiTheSloth <varszegilacko@gmail.com>
2024-12-10 17:00:45 +0100

    Continued section 7

--- Section_7.py
+++ Section_7.py
@@ -1,15 +1,93 @@
 import random
 
+hangman_stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-guess = str(input("Take a guess! ")).lower()
-print(guess)
+placeholder = ""
+for cnt in range(0, len(chosen_word)):
+    placeholder += "_"
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+print(placeholder)
+game_over = False
+correct_letters = []
+lives = 6
+
+while not game_over:
+    guess = str(input("Take a guess! ")).lower()
+    print(guess)
+
+    display = ""
+
+    for char in chosen_word:
+        if char == guess:
+            display += char
+            correct_letters.append(char)
+        elif char in correct_letters:
+            display += char
+        else:
+            display += "_"
+            lives -= 1
+
+    print(hangman_stages[lives])
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
+
+    if lives == 0:
+        print("You lose")
+        break
