796ff46508169ea4a27ad55eca9d5eab3662aed9
vlaciiTheSloth <varszegilacko@gmail.com>
2024-12-18 16:44:15 +0100

    Added plus functionalities to section 7's project

--- Section_7_hangman/Section_7.py
+++ Section_7_hangman/Section_7.py
@@ -13,14 +13,23 @@ for cnt in range(0, len(chosen_word)):
 print(placeholder)
 game_over = False
 correct_letters = []
+wrong_guesses = []
 lives = 6
 
 while not game_over:
-    print(f"******************** {lives}/6 LIVES LEFT ********************")
+    print(f"\n******************** {lives}/6 LIVES LEFT ********************")
     guess = str(input("Take a guess! ")).lower()
     print(guess)
     if guess in correct_letters:
-        print("You've already guessed this letter: " + guess)
+        print("You've already guessed this letter: '" + guess + "'.")
+    elif len(guess) == len(chosen_word) and guess == chosen_word:
+        print("Are you late from somewhere? Nevermind, you got the word! You won!")
+        print(Section_7_hm_art.you_won)
+        game_over = True
+        break
+    elif len(guess) > 1:
+        print("Please guess make a guess on one single letter.")
+
 
     display = ""
 
@@ -36,15 +45,20 @@ while not game_over:
     print(display)
 
     if guess not in chosen_word:
-        lives -= 1
-        print("You guessed " + guess + ", which is not in the word. You lose a life.")
-        if lives == 0:
-            game_over = True
-            print(f"You lose. The word was: {chosen_word}")
-
+        if guess in wrong_guesses:
+            print("You've already guessed the letter '" + guess + ",' which is not in the word.")
+        else:
+            lives -= 1
+            print("You guessed " + guess + ", which is not in the word. You lose a life.")
+            if lives == 0:
+                game_over = True
+                print(f"You lose. The word was: {chosen_word}")
+        wrong_guesses += guess
+ 
     if "_" not in display:
         game_over = True
-        print("You win")
+        print("You win!")
+        print(Section_7_hm_art.you_won)
 
 
     print(Section_7_hm_art.hangman_stages[lives])
