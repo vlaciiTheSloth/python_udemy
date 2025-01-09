599a9ce536953bf6dd052ae1aa917847e55e3567
vlaciiTheSloth <varszegilacko@gmail.com>
2025-01-09 11:35:12 +0100

    Finished Section 8

--- Section_8_caesar_cipher.py
+++ Section_8_caesar_cipher.py
@@ -1,44 +1,32 @@
 import string
 
 alphabet = list(string.ascii_lowercase)
+should_continue = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(encode_or_decode, original_text, shift_amount):
+        output_text = ""
+        if encode_or_decode == "decode":
+            shift_amount *= -1
 
-
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
-    for character in original_text:
-        shifted_position = alphabet.index(character) + shift_amount
-        shifted_position %= len(alphabet)
-        encrypted_text += alphabet[shifted_position]
-    print(f"Encypted text:{encrypted_text}")
-
-def decrypt(original_text, shift_amount):
-    decrypted_text = ""
-    for character in original_text:
-        shifted_position = alphabet.index(character) - shift_amount
-        decrypted_text += alphabet[shifted_position]
-    print(f"Decrypted text: {decrypted_text}")
-
-def caesar(method, original_text, shift_amount):
-    if method == "encode":
-        encrypted_text = ""
         for character in original_text:
-            shifted_position = alphabet.index(character) + shift_amount
-            shifted_position %= len(alphabet)
-            encrypted_text += alphabet[shifted_position]
-        print(f"Encypted text: {encrypted_text}")
-    elif method == "decode":
-         decrypted_text = ""
-         for character in original_text:
-            shifted_position = alphabet.index(character) - shift_amount
-            decrypted_text += alphabet[shifted_position]
-         print(f"Decrypted text: {decrypted_text}")
-    else:
-        print("Invalid input.")
+            if character not in alphabet:
+                  output_text += character
+            else:
+                shifted_position = alphabet.index(character) + shift_amount
+                shifted_position %= len(alphabet)
+                output_text += alphabet[shifted_position]
+                
+        print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-#encrypt(original_text = text, shift_amount = shift)
-#decrypt(original_text = text, shift_amount = shift)
-caesar(method = direction, original_text = text, shift_amount = shift)
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(encode_or_decode = direction, original_text = text, shift_amount = shift)
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n").lower()
+
+    if restart == "no":
+        should_continue = False
+        print("Goodbye!")
